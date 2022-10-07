43d34b4756bd9498e047e6aa8e8ea25596d7a3ac
Harlon Garcia <harlondesigner@gmail.com>
2022-10-06 23:02:15 -0300

    feat: adding agent role description

--- src/pages/Agent/Agent.jsx
+++ src/pages/Agent/Agent.jsx
@@ -24,32 +24,39 @@ const Agent = () => {
 
   if (!agent) return;
   return (
-    <div className="flex flex-col bg-blue-dark h-full w-full items-center justify-center p-8">
+    <div className="flex flex-col bg-blue-dark h-full w-full items-center justify-center p-8 sm:py-14">
       {loading && <LoadingSpinner />}
       <div className="max-w-2xl lg:max-w-3xl">
         {!loading && !error && (
-          <div className="flex flex-col items-center sm:grid sm:grid-cols-2 sm:px-8">
-            <h2 className="text-red font-montserrat text-3xl font-bold inline-block mb-4 sm:text-5xl md:text-6xl md:mb-0">
+          <div className="flex flex-col items-center sm:grid sm:grid-cols-2 sm:px-4 lg:px-0">
+            <h2 className="text-red font-montserrat text-3xl font-bold inline-block mb-4 sm:text-4xl lg:text-5xl md:mb-2 lg:mb-0">
               {agent.displayName}
             </h2>
             <img
-              className="mb-8 px-4 row-span-3 sm:mb-0 lg:row-span-3"
+              className="mb-8 sm:pl-4 row-span-3 sm:mb-0 lg:row-span-3 max-h-96 sm:max-h-max"
               src={agents_images[agent.displayName]}
               alt={agent.displayName}
             />
 
-            <div className="flex flex-col items-center mb-8 row-span-0 sm:flex-row sm:mb-4 md:mb-0 lg:bg-red lg:py-2 lg:px-4 lg:w-fit">
-              <h3 className="text-red text-xl font-montserrat inline-block mb-2 sm:mb-0 sm:mr-4 sm:text-2xl lg:text-white lg:text-4xl">
-                {agent.role.displayName}
-              </h3>
-              <img
-                className="w-12 h-12"
-                src={agent.role.displayIcon}
-                alt={agent.role.displayName}
-              />
+            <div className="flex flex-col row-span-0 mb-8 sm:mb-4 sm:py-4 sm:border-b-2 sm:border-blue-light">
+              <div className="flex flex-col items-center sm:flex-row sm:mb-4 lg:bg-red lg:py-2 lg:px-4 lg:w-fit">
+                <h3 className="text-blue-light text-xl font-montserrat inline-block mb-2 sm:mb-0 sm:mr-2 sm:text-2xl lg:text-xl lg:font-semibold lg:text-white ">
+                  {agent.role.displayName}
+                </h3>
+                <img
+                  className="w-8 h-8 lg:w-6 lg:h-6 "
+                  src={agent.role.displayIcon}
+                  alt={agent.role.displayName}
+                />
+              </div>
+              <p className="w-full font-montserrat text-white hidden sm:block lg:text-lg md:mb-4 lg:mb-8">
+                {agent.role.description}
+              </p>
             </div>
-
-            <p className="text-white text-lg text-center font-montserrat sm:text-left lg:text-xl">
+            <p className="text-white text-center font-montserrat sm:text-left lg:text-lg">
+              <span className="text-red lg:text-xl font-medium">
+                Biografia -
+              </span>{" "}
               {agent.description}
             </p>
           </div>
